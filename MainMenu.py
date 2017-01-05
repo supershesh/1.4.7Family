c326d0cd8b8a7f0753f026d928319ac997eec3be
Scot Meldrum <18smeldrum@gmail.com>
2017-01-05 14:23:20 -0700

    Added branching for the individual options. All that's needed is the functions to make this work now.

--- MainMenu.py
+++ MainMenu.py
@@ -4,9 +4,11 @@ def editPicture():
     takes no parameters. The program will ask you for needed parameters
     when you choose an option.'''
     #initializes menuOption to 0: a value that shouldn't ever be used except for
-    #here.
+    #here
+    
     menuOption = 0
     while menuOption is not 4:
+        
         #Gives the user a chance to input and tells them what each input will do.
         try:
             print('\nEnter 1 for Adding a Border\n\n'
@@ -18,6 +20,18 @@ def editPicture():
             #Now that the user has chosen which input, test if it is in range.
             if menuOption < 1 or menuOption > 4:
                 print('You must enter a number between 1 and 4. Try again.')
+                
+            elif menuOption == 1:
+                print('something1')
+                #Insert border function here
+                
+            elif menuOption == 2:
+                print('something2')
+                #Insert watermark function here
+                
+            elif menuOption == 3:
+                print('something3')
+                #Insert code to run both programs
             
         #If the user didn't enter a number, which will cause the program to be annoyed...
         except ValueError:
