18864d0eff92a0c3b16e98d91555b133783b2838
Austin "Nohrian Scum" Johnson <leonarth.aj@gmail.com>
2017-01-09 14:35:54 -0700

    Added functionality with file finder

--- Austin.py
+++ Austin.py
@@ -5,32 +5,48 @@ import matplotlib.pyplot as plt
 import os.path
 import numpy as np 
 
-'''Read the image data'''
-# Get the directory of this python script
-directory = os.path.dirname(os.path.abspath(__file__)) 
-# Build an absolute filename from directory + filename
-filename = os.path.join(directory, 'woman.gif')
-# Read the image data into an array
-img = plt.imread(filename)
+import PIL
+import os.path  
 
-height = len(img)
-width = len(img[0])
-#Check for proper extension
-if filename.endswith('.gif'):
+def get_images(directory=None):
+ ''' Returns PIL.Image objects for all the images in directory.
+
+ If directory is not specified, uses current directory.
+ Returns a 2-tuple containing 
+ a list with a PIL.Image object for each image file in root_directory,     
+ and a list with a string filename for each image file in root_directory
+ '''
+
+ if directory == None:
+     directory = os.getcwd() # Use working directory if unspecified
+
+ image_list=[] # Initialize aggregators
+ file_list = []
+
+ directory_list = os.listdir(directory) # Get list of files
+ for entry in directory_list:
+    absolute_filename = os.path.join(directory, entry)
     try:
-        for row in range (height):
-            for column in range (width):
-                x = img[row][column][0]
-                y = img[row][column][1]
-                z = img[row][column][2]
-                img[row][column] = [x,y,z,128]
-
+         image = PIL.Image.open(absolute_filename)
+         file_list += [entry]
+         image_list += [image]
+         filename = os.path.join(directory, absolute_filename)
+         img = plt.imread(filename)
+         height = len(img)
+         width = len(img[0])
+         for row in range (height):
+                for column in range (width):
+                    x = img[row][column][0]
+                    y = img[row][column][1]
+                    z = img[row][column][2]
+                    img[row][column] = [x,y,z,128]
+      
     except IOError: 
         pass
-else:
-    print "Wrong file extension used, the image opacity won't be changed!"
-    
-#Give a warning if the wrong file extension is used
+    else:
+        print "Wrong file extension used, the image opacity won't be changed!"
+    #Give a warning if the wrong file extension is used
+
 
 
 '''Show the image data'''
